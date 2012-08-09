547351397a93032ae4a56dd1ba7d48392d564042
Tako Neko <oct8cat@gmail.com>
2012-08-09 14:08:00 +0400

    Templates is now used to format PHPDoc.

--- PhpDox.py
+++ PhpDox.py
@@ -10,25 +10,24 @@ class PhpdoxCommand(sublime_plugin.TextCommand):
     patterns = {
         'class': r"""
             ^\s*                                        # indent
-            class\s+                                    # "class" in the beginning of line
-            (?P<c_name>\w+)                             # class name
-            (
-                \s*$                                    # line end
-                |\s+extends\s+(?P<pc_name>\w+)          # or "extends" followed by parent class name
-            )
+            class\s+                                    # "class"
+            (?P<name_class>\w+)\s*                      # class name
+            (extends|)\s*                               # "extends"
+            (?P<name_parent>\w+|\s*)                    # parent class name
+            .*
         """,
         'function': r"""
             ^\s*                                        # indent
-            (?P<access>public|protected|private|var)\s* # access
+            (?P<access>public|protected|private|)\s* # access
             (?P<static>static|)\s*                      # static modifier
-            function\s                                  # "function"
+            function\s+                                 # "function"
             (?P<name>\w+)\s*                            # name
             \((?P<params>.*)\)                          # parameters
             .*
         """,
-        'var': r"""
+        'variable': r"""
             ^\s*                                        # indent
-            (?P<access>public|protected|private|)\s*    # access
+            (?P<access>public|protected|private|var)\s*    # access
             (?P<static>static|)\s*                      # static modifier
             (?P<name>\$\w+)\s*                          # name
             (\s*=\s*|)
@@ -37,7 +36,48 @@ class PhpdoxCommand(sublime_plugin.TextCommand):
         """,
     }
 
+    templates = {
+
+# Class template
+        'class':"""
+/**
+* ${{1:{0}}}
+*
+* @uses     {1}
+*
+* @category ${{2:Category}}
+* @package  ${{3:Package}}
+* @author   ${{TM_FULLNAME}} <${{TM_EMAIL}}>
+* @license  GNU GPL v3.0 {{@link http://www.gnu.org/copyleft/gpl.html}}
+* @link     ${{4:http://www.example.com}}
+**/""",
+
+# Function template
+        'function':"""
+/**
+ * ${{1:{name}}}
+ *
+{params}
+ * @access {access}
+{static}
+ *
+ * @return ${{2:mixed}} ${{3:Value}}.
+ */""",
+
+# Variable template
+        'variable':"""
+/**
+ * ${{1:\\{name}}}
+ *
+ * @var {type}
+ *
+ * @access {access}
+{static}
+ */"""
+    }
+
     def run(self, edit, **args):
+        """Entry point"""
         v = self.view
         line = v.line(v.sel()[0])
         snippet = self.process_line(v.substr(line))
@@ -46,6 +86,7 @@ class PhpdoxCommand(sublime_plugin.TextCommand):
             v.run_command('insert_snippet', {'contents': snippet})
 
     def process_line(self, line):
+        """Resolves current line type and calls respective parse method"""
         for p_name, p_string in self.patterns.iteritems():
             pattern = re.compile(p_string, re.VERBOSE)
             match = pattern.search(line)
@@ -54,67 +95,61 @@ class PhpdoxCommand(sublime_plugin.TextCommand):
                 return method(match)
         return None
 
+
+
     def dox_class(self, match):
-        c_name, pc_name = match.group('c_name', 'pc_name')
-        snippet =  '/**\n'
-        snippet += ' * ${1:' + c_name + '}.\n'
-        snippet += ' *\n'
-        if (pc_name != None):
-            snippet += ' * @uses     ' + pc_name +'\n'
-            snippet += ' *\n'
-        snippet += ' * @category ${2:Category}\n'
-        snippet += ' * @package  ${3:Package}\n'
-        snippet += ' * @author   ${TM_FULLNAME} <${TM_EMAIL}>\n'
-        snippet += ' * @license  ${TM_LICENSE}\n'
-        snippet += ' * @link     http://www.example.com/\n'
-        snippet += ' **/'
-        return snippet
+        """Resolves class's PHPDoc by given match"""
+        return self.templates['class'].format(*match.group('name_class', 'name_parent'))
 
     def dox_function(self, match):
-        access, static, name, params = match.group('access', 'static', 'name', 'params')
-        if (access == ''):
-            access = 'public'
-        snippet =  '/**\n'
-        snippet += ' * ${1:' + name + '}.\n'
-        snippet += ' *\n'
-        if (params != ''):
-            # TODO parameters alignment
-            for param in params.split(','):
-                p_name, eq, initial = param.replace(' ', '').partition('=')
-                p_type = self.resolve_var_type(initial)
-                snippet += ' * @param ' + p_type + ' \\' + p_name + ' Description.\n'
-            snippet += ' *\n'
+        """Resolves function's PHPDoc by given match"""
+        tokens = {
+            'access': self.resolve_access(match.group('access')),
+            'static': self.resolve_static(match.group('static')),
+            'name': match.group('name'),
+            'params': self.resolve_params(match.group('params')),
+        }
+        return self.templates['function'].format(**tokens)
 
-        snippet += ' * @access ' + access + '\n'
-        if (static != ''):
-            snippet += ' * @static\n'
-        snippet += ' *\n'
-        snippet += ' * @return mixed Value.\n'
-        snippet += ' **/'
-        return snippet
+    def dox_variable(self, match):
+        """Resolves variable's PHPDoc by given match"""
+        tokens = {
+            'access': self.resolve_access(match.group('access')),
+            'static': self.resolve_static(match.group('static')),
+            'name': match.group('name'),
+            'type': self.resolve_var_type(match.group('value')),
+        }
+        return self.templates['variable'].format(**tokens)
 
-    def dox_var(self, match):
-        access, static, name, value = match.group('access', 'static', 'name', 'value')
-        type = self.resolve_var_type(value)
-        if (access == ''):
-            access = 'public'
-        snippet =  '/**\n'
-        snippet += ' * ${1:\\' + name + '}.\n'
-        snippet += ' *\n'
-        snippet += ' * @var ' + type + '\n'
-        snippet += ' *\n'
-        snippet += ' * @access ' + access + '\n'
-        if (static != ''):
-            snippet += ' * @static\n'
-        snippet += ' **/'
-        return snippet
 
-    def resolve_var_type(self, var):
+
+    def resolve_access(self, val):
+        """Resolves access modifier value"""
+        if (val != ''):
+            return val
+        return 'public'
+
+    def resolve_static(self, val):
+        """Resolves static modifier value"""
+        if (val == 'static'):
+            return ' * @static'
+        return ''
+
+    def resolve_params(self, val):
+        """Resolves method's parameters description"""
+        ret = ''
+        for param in val.replace(' ', '').split(','):
+            name, assign, value = param.partition('=')
+            ret += ' * @param {0} \\{1} Description.\n'.format(self.resolve_var_type(value), name)
+        return ret + ' *'
+
+    def resolve_var_type(self, val):
+        """Resolves variable's type by given value"""
         type = 'mixed'
-        if (var == 'array()'):
+        if (val == 'array()'):
             type = 'array'
-        elif (var.isdigit()):
+        elif (val.isdigit()):
             type = 'int'
-        elif (var.startswith(('\'', '"'))):
+        elif (val.startswith(('\'', '"'))):
             type = 'string'
         return type
